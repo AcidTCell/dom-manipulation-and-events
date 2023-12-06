8fdbe5a372dc58651e89f69cdc064af491d53b19
AcidTCell <cimafrancajasper.personal@gmail.com>
2023-12-07 00:30:35 +0800

    Add function to store user input data

---
+++ script.js
@@ -0,0 +1,14 @@
+const unorderedList = document.querySelector('ul');
+const userInput = document.querySelector('input');
+const btn = document.querySelectorAll('button');
+
+btn.forEach((button)=>{
+    button.addEventListener('click', () =>{
+        const input = document.getElementById('item');
+        const inputValue = input.value;
+        console.log(inputValue);
+
+
+    });
+
+});
