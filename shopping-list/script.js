2017d4cbce451809ba94b641b299809a9a3d5393
AcidTCell <cimafrancajasper.personal@gmail.com>
2023-12-13 07:38:02 +0800

    Create directory and add keyboard event prop file

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelectorAll('button');
 
 btn.forEach((button)=>{
     button.addEventListener('click', () =>{
-        const input = document.getElementById('item');
+        const input = document.querySelector('#item');
         const inputValue = input.value;
         console.log(inputValue);
         input.value = '';
